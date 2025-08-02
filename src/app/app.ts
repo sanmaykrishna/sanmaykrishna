c2fdd7208a04d978ac48983c4b470b05b16d6056
sanmay2002 <sanmaykrishna@gmail.com>
2025-08-02 18:19:50 +0530

    dasd

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pic } from './pic/pic';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Pic],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
